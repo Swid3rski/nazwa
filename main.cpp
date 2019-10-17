f05199482bdc6f5a7e105108a6154bf3346b877b
Kacper <swiderski.kacper99@gmail.com>
2019-10-17 13:01:59 +0200

    podstawa

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
 
-int main(int argc, char** argv) {
-	int liczba = 55
-	int 
-	return 0;
-	sfsf
-	sfaf
+int main()
+{
+    long bolivary, PLN;
+    cout << "Podaj ile masz bolivarow :"; cin >> bolivary;
+    PLN = bolivary/65038.54;
+    cout << "Masz :" << PLN << "PLN";
+
+
+    return 0;
 }
