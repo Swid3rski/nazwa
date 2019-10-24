30cd9124cb56c9ef3af2011300d51842f2a43bd3
Kacper <swiderski.kacper99@gmail.com>
2019-10-24 12:38:03 +0200

    euro

--- main.cpp
+++ main.cpp
@@ -4,10 +4,15 @@ using namespace std;
 
 int main()
 {
-    long bolivary, PLN;
-    cout << "Podaj ile masz bolivarow :"; cin >> bolivary;
+    long bolivary, PLN, GBP, PLN2;
+    cout << "Podaj ile masz bolivarow :"; 
+    cin >> bolivary;
+    cout << "Podaj ile masz funtow :";
+    cin >> GBP;
     PLN = bolivary/65038.54;
+    PLN2 = GBP/4,96025161;
     cout << "Masz :" << PLN << "PLN";
+    cout << "Masz :" << PLN2 
 
 
     return 0;
